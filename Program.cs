0f2ea4268779936844f73cfcfa00fe18cba2bdc5
Ryan Kooy <rwkooy@gmail.com>
2020-03-20 16:02:13 -0400

    punches

--- Program.cs
+++ Program.cs
@@ -6,43 +6,35 @@ namespace HoursAccrued
 {
     class Program
     {
+        public static Excel.Application xP = new Excel.Application();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Type \"start\" to begin recording your time . . .");
             string start = Console.ReadLine().ToLower();
             if (start == "start")
             {
+                string path = "C:\\Users\\Ry\\Desktop\\WorkHours.txt";
                 DateTime now = DateTime.Now;
-                LogTime(now);
+                InPunch(now, path);
                 Console.WriteLine("Do not close this window!\nType \"stop\" when you are done . . .");
                 string stop = Console.ReadLine().ToLower();
                 if (stop == "stop")
                 {
                     DateTime newNow = DateTime.Now;
-                    LogTime(newNow);
                     TimeSpan diff = newNow.Subtract(now);
-                    Console.WriteLine($"You've worked {diff.Hours} hours, {diff.Minutes} minutes.");
-					string path = "C:\\Users\\Ry\\Desktop\\WorkHours.txt";
-					if (!File.Exists(path))
-					{
-                        using StreamWriter sw = File.CreateText(path);
-						sw.WriteLine($"{diff.Hours}:{diff.Minutes}");
-					}
-					else
-					{
-                        using StreamWriter sw = File.AppendText(path);
-                        string punch = $"PUNCH: {newNow.Hour}:{newNow.Minute}\n>{diff.Minutes} worked";
-                        sw.WriteLine(punch);
-                        Console.WriteLine(punch);
-                    }
-                    try
-                    {
-                        ExcelWrite(diff.Minutes);
-                    }
-                    catch (FileNotFoundException e)
-                    {
-                        File.Create(e.FileName);
-                    }
+                    string punch = $"[OUT-PUNCH: {newNow.Hour}:{newNow.Minute}]\n{diff.Minutes} minutes worked\n";
+                    using StreamWriter sw = File.AppendText(path);
+                    sw.WriteLine(punch);
+                    /*try
+                     *{
+                     *    ExcelWrite(diff.Minutes);
+                     *}
+                     *catch (FileNotFoundException e)
+                     *{
+                     *    File.Create(e.FileName);
+                     *}
+                     */
                 }
                 else
                 {
@@ -55,20 +47,30 @@ namespace HoursAccrued
             }
         }
 
-        static void LogTime(DateTime now)
+        static void InPunch(DateTime now, string path)
         {
-            Console.WriteLine($"Current time: {now.Hour}:{now.Minute}:{now.Second}");
+            string punch = $"[IN-PUNCH: {now.Hour}:{now.Minute}]\n| | |\nv v v";
+            if (!File.Exists(path))
+            {
+                using StreamWriter sw = File.CreateText(path);
+                sw.WriteLine(punch);
+            }
+            else
+            {
+                using StreamWriter sw = File.AppendText(path);
+                sw.WriteLine(punch);
+            }
+            Console.WriteLine(punch);
         }
 
         static void ExcelWrite(int min)
         {
-            string xPath = "C:\\Users\\Ry\\Desktop\\HoursAccrued2.xlsx";
-            Excel.Application xP = new Excel.Application();
+            string xPath = "C:\\Users\\Ry\\Desktop\\HoursAccrued3.xlsx";
             Excel.Workbook xB = xP.Workbooks.Open(xPath);
             Excel.Worksheet? xS = xP.ActiveSheet as Excel.Worksheet;
-            Excel.Range cell = xS.Range["A1"];
-            int total = (int)cell.Value;
-            xS.Cells["A1"] = total + min;
+            // Excel.Range cell = xS.Range["A1"];
+            // int total = (int)cell.Value;
+            xS.Cells["A4"] = min;
             xB.Close();
             xP.Quit();
         }
