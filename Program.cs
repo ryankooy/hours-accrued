b96e112ce3814d0fe05308423081c6b23ef0b765
Ryan Kooy <rwkooy@gmail.com>
2020-03-21 01:34:52 -0400

    'autocommit'

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace HoursAccrued
@@ -14,17 +15,22 @@ namespace HoursAccrued
             string start = Console.ReadLine().ToLower();
             if (start == "start")
             {
-                string path = "C:\\Users\\Ry\\Desktop\\WorkHours.txt";
+                string path = @"C:\Users\Ry\Desktop\WorkHours.txt";
+                string lastTotal = File.ReadLines(path).Last();
                 DateTime now = DateTime.Now;
                 InPunch(now, path);
-                Console.WriteLine("Do not close this window!\nType \"stop\" when you are done . . .");
+                Console.WriteLine("Do not close this window!\nType \"stop\" when you're done . . .");
                 Console.ReadLine();
                 DateTime newNow = DateTime.Now;
                 TimeSpan diff = newNow.Subtract(now);
                 double hours = Math.Round(((int)diff.Minutes / 60.00), 2);
-                string punch = $"[OUT-PUNCH: {newNow.Hour}:{newNow.Minute}]\nHOURS WORKED: {hours}\n";
+                string punch = $"[OUT-PUNCH: {newNow.Hour}:{newNow.Minute}]\nHOURS WORKED:\n{hours}";
+                double total = Convert.ToDouble(lastTotal);
+                double newTotal = Math.Round(total + hours, 2);
                 using StreamWriter sw = File.AppendText(path);
                 sw.WriteLine(punch);
+                sw.WriteLine("NEW TOTAL:");
+                sw.WriteLine(newTotal);
                 /*try
                     *{
                     *    ExcelWrite(diff.Minutes);
@@ -43,7 +49,7 @@ namespace HoursAccrued
 
         static void InPunch(DateTime now, string path)
         {
-            string punch = $"[IN-PUNCH: {now.Hour}:{now.Minute}]";
+            string punch = $"\n\n[IN-PUNCH: {now.Hour}:{now.Minute}]";
             if (!File.Exists(path))
             {
                 using StreamWriter sw = File.CreateText(path);
@@ -59,7 +65,7 @@ namespace HoursAccrued
 
         static void ExcelWrite(int min)
         {
-            string xPath = "C:\\Users\\Ry\\Desktop\\HoursAccrued3.xlsx";
+            string xPath = @"C:\Users\Ry\Desktop\HoursAccrued3.xlsx";
             Excel.Workbook xB = xP.Workbooks.Open(xPath);
             Excel.Worksheet? xS = xP.ActiveSheet as Excel.Worksheet;
             // Excel.Range cell = xS.Range["A1"];
