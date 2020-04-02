cdfd597111d4b9ec990dcdec23d0d63eca144322
Ryan Kooy <rwkooy@gmail.com>
2020-04-02 19:41:33 -0400

    'autocommit'

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using Excel = Microsoft.Office.Interop.Excel;
+// using Excel = Microsoft.Office.Interop.Excel;
 
 namespace HoursAccrued
 {
     class Program
     {
-        public static Excel.Application xP = new Excel.Application();
-
         static void Main(string[] args)
         {
             Console.WriteLine("Type \"start\" to begin recording your time . . .");
@@ -24,23 +22,13 @@ namespace HoursAccrued
                 DateTime newNow = DateTime.Now;
                 TimeSpan diff = newNow.Subtract(now);
                 double hours = Math.Round(((int)diff.Hours + ((int)diff.Minutes / 60.00)), 2);
-                string punch = $"[OUT-PUNCH: {newNow.Hour}:{newNow.Minute}]\nHOURS WORKED:\n{hours}";
+                string punch = $"[OUT-PUNCH: {newNow.Hour}:{newNow.Minute} {newNow.DayOfWeek}]\nHOURS WORKED:\n{hours}";
                 double total = Convert.ToDouble(lastTotal);
                 double newTotal = Math.Round(total + hours, 2);
                 using StreamWriter sw = File.AppendText(path);
-                sw.WriteLine($"Test: {diff.Hours} plus {diff.Minutes / 60.00} equals {hours}");
                 sw.WriteLine(punch);
                 sw.WriteLine("NEW TOTAL:");
                 sw.WriteLine(newTotal);
-                /*try
-                    *{
-                    *    ExcelWrite(diff.Minutes);
-                    *}
-                    *catch (FileNotFoundException e)
-                    *{
-                    *    File.Create(e.FileName);
-                    *}
-                    */
             }
             else
             {
@@ -50,7 +38,7 @@ namespace HoursAccrued
 
         static void InPunch(DateTime now, string path)
         {
-            string punch = $"\n[IN-PUNCH: {now.Hour}:{now.Minute}]";
+            string punch = $"\n[IN-PUNCH: {now.Hour}:{now.Minute} {now.DayOfWeek}]";
             if (!File.Exists(path))
             {
                 using StreamWriter sw = File.CreateText(path);
@@ -62,18 +50,6 @@ namespace HoursAccrued
                 sw.WriteLine(punch);
             }
             Console.WriteLine(punch);
-        }
-
-        static void ExcelWrite(int min)
-        {
-            string xPath = @"C:\Users\Ry\Desktop\HoursAccrued3.xlsx";
-            Excel.Workbook xB = xP.Workbooks.Open(xPath);
-            Excel.Worksheet? xS = xP.ActiveSheet as Excel.Worksheet;
-            // Excel.Range cell = xS.Range["A1"];
-            // int total = (int)cell.Value;
-            xS.Cells["A4"] = min;
-            xB.Close();
-            xP.Quit();
         }
     }
 }
