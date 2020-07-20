8474e9767e77f8219d02a4a4f3c692f971722b21
Ryan Kooy <rwkooy@gmail.com>
2020-07-20 07:50:06 -0400

    'autocommit'

--- Program.cs
+++ Program.cs
@@ -1,38 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-// using Excel = Microsoft.Office.Interop.Excel;
 
 namespace HoursAccrued
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Enter \"start\" to begin recording your time.");
             string start = Console.ReadLine().ToLower();
             if (start == "start")
             {
                 string path = @"C:\Users\Ry\Desktop\WorkHours.txt";
-                string lastTotal = File.ReadLines(path).Last();
                 DateTime now = DateTime.Now;
+                string lastTotal = File.ReadLines(path).Last();
                 InPunch(now, path);
                 Console.WriteLine("Do not close this window!\nEnter \"stop\" when you're done . . .");
                 Console.ReadLine();
-                Console.WriteLine('.');
-                Console.WriteLine(". .");
-                Console.WriteLine(". . .");
-                Console.WriteLine(". . . .");
-                Console.WriteLine(". . . . .");
-                Console.WriteLine(". . . .");
-                Console.WriteLine(". . .");
-                Console.WriteLine(". .");
-                Console.WriteLine('.');
                 Console.WriteLine("Great work!");
                 DateTime newNow = DateTime.Now;
                 TimeSpan diff = newNow.Subtract(now);
                 double hours = Math.Round(((int)diff.Hours + ((int)diff.Minutes / 60.00)), 2);
-                string punch = $"[OUT-PUNCH: {newNow.Hour}:{newNow.Minute} {newNow.Month}/{newNow.Day} {newNow.DayOfWeek}]\n+ {hours} hrs";
+                string punch = $"[OUT-PUNCH: {newNow.Hour}:{newNow.Minute} ◦ {newNow.Month}/{newNow.Day} ◦ {newNow.DayOfWeek}]\n+ {hours} hrs";
                 double total = Convert.ToDouble(lastTotal);
                 double newTotal = Math.Round(total + hours, 2);
                 using StreamWriter sw = File.AppendText(path);
@@ -41,14 +31,21 @@ namespace HoursAccrued
                 sw.WriteLine(newTotal);
             }
             else
-            {
                 Console.WriteLine("Seriously?");
-            }
         }
 
         static void InPunch(DateTime now, string path)
         {
-            string punch = $"\n[IN-PUNCH: {now.Hour}:{now.Minute} {now.Month}/{now.Day} {now.DayOfWeek}]";
+            Console.WriteLine('.');
+            Console.WriteLine(". .");
+            Console.WriteLine(". . .");
+            Console.WriteLine(". . . .");
+            Console.WriteLine(". . . . .");
+            Console.WriteLine(". . . .");
+            Console.WriteLine(". . .");
+            Console.WriteLine(". .");
+            Console.WriteLine('.');
+            string punch = $"\n[IN-PUNCH: {now.Hour}:{now.Minute} ◦ {now.Month}/{now.Day} ◦ {now.DayOfWeek}]";
             if (!File.Exists(path))
             {
                 using StreamWriter sw = File.CreateText(path);
